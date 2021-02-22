07a14b38fa6bd88dd14d3276728f3d53f1e4769c
utkarshgubrelay <utkarshgubrelay2000@gmail.com>
2021-02-23 00:06:56 +0530

    image button added

--- src/App.js
+++ src/App.js
@@ -104,6 +104,20 @@ function App() {
 </div>
    </section>
 
+   <section type="section" className="btn cta-btn m-1">
+    <div className="form-check">
+
+    <input type="file" class="form-control-file" id="exampleFormControlFile1" style={{display:'none'}}/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-plus" viewBox="0 0 16 16">
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>
+  <label className="form-check-label" for="exampleRadios1">
+  Image
+  </label>
+</div>
+   </section>
+
+
     </div>
 </div>
 </div>
